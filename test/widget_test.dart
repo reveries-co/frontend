58ed1c36645bbf77260012e7bd8134e096f689eb
Pedro Araujo <pedromcaraujo@gmail.com>
2022-03-06 17:45:52 +0000

    Remove build folder and add .gitignore

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:reveries-app/main.dart';
+import 'package:reveries_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
