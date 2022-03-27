4fb30201a24ad5a4b02fb60083e49667843c4219
Pedro Araujo <pedromcaraujo@gmail.com>
2022-03-27 18:57:25 +0100

    Make the authenticated redirects work properly

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,17 +1,70 @@
+
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:provider/provider.dart';
+import 'package:reveries_app/screens/demo.dart';
 
-import '../services/auth_service.dart';
+import '../blocs/auth_block.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  State createState() => _LoginScreenState();
+
+  static const routeName = '/login';
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late StreamSubscription<User?> loginStateSubscription;
+
+  @override
+  void initState() {
+    var authBloc = Provider.of<AuthBloc>(context, listen: false);
+    loginStateSubscription = authBloc.currentUser.listen((gUser) {
+      if (gUser != null) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => DemoScreen(),
+          ),
+        );
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    loginStateSubscription.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final authService = Provider.of<AuthService>(context);
-    if (authService.user != null) {
-      // user already authenticated let's redirect to main screen
-      Navigator.of(context).pushNamed("/");
-    }
-    return Text("Login");
+    final authBloc = Provider.of<AuthBloc>(context);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Google Sign In'),
+        ),
+        body: ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: Scaffold(
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SignInButton(
+                      Buttons.Google,
+                      onPressed: () => authBloc.loginGoogle(),
+                    ),
+                  ],
+                ),
+              )),
+        ));
   }
+
+
+
 }
